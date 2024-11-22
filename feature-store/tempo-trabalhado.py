9773d278dc885e8ae69cd15784a7d0bceb5ed69f
r0bert <robertdsgabriel@gmail.com>
2024-11-22 20:58:47 -0300

    Enhance tempo-trabalhado script to calculate and format user balance, including positive and negative saldo summaries for January 2024

--- feature-store/tempo-trabalhado.py
+++ feature-store/tempo-trabalhado.py
@@ -60,8 +60,80 @@ saldo_pos
 tempo_trabalhado_df['saldo'] = tempo_trabalhado_df['tempo_trabalhado'].apply(lambda x: (pd.Timedelta(x) - pd.Timedelta(hours=8)).total_seconds() / 3600)
 tempo_trabalhado_df['saldo'] = tempo_trabalhado_df['saldo'].apply(lambda x: str(pd.Timedelta(hours=x)).split(' ')[-1] if x >= 0 else '-' + str(pd.Timedelta(hours=abs(x))).split(' ')[-1])  # Convert to hh:mm:ss format
 
-# Round seconds to 2 decimal places
-tempo_trabalhado_df['saldo'] = tempo_trabalhado_df['saldo'].apply(lambda x: ':'.join([part if i < 2 else f"{float(part):05.2f}" for i, part in enumerate(x.split(':'))]))
+# Remove milliseconds
+tempo_trabalhado_df['saldo'] = tempo_trabalhado_df['saldo'].apply(lambda x: ':'.join(x.split(':')[:2] + [x.split(':')[2].split('.')[0]]))
 
 tempo_trabalhado_df
+# %%
+saldo_positivo_df = tempo_trabalhado_df[tempo_trabalhado_df['saldo'].apply(lambda x: not x.startswith('-'))]
+saldo_negativo_df = tempo_trabalhado_df[tempo_trabalhado_df['saldo'].apply(lambda x: x.startswith('-'))]
+
+
+saldo_positivo_df
+#%%
+saldo_negativo_df
+# %%
+mes_negativo = saldo_negativo_df[saldo_negativo_df['date'].str.contains("-01-2024")]
+mes_positivo = saldo_positivo_df[saldo_positivo_df['date'].str.contains("-01-2024")]
+# %%
+mes_positivo
+mes_negativo
+# %%
+# Somar os valores do saldo para cada usuário no mês positivo
+mes_positivo['saldo'] = mes_positivo['saldo'].apply(lambda x: pd.Timedelta(x).total_seconds() / 3600)
+saldo_final_positivo = mes_positivo.groupby('pin')['saldo'].sum().reset_index()
+
+# Converter o saldo final de volta para o formato hh:mm:ss
+saldo_final_positivo['saldo'] = saldo_final_positivo['saldo'].apply(lambda x: ':'.join(str(pd.Timedelta(hours=x)).split(' ')[-1].split(':')[:2] + [str(pd.Timedelta(hours=x)).split(' ')[-1].split(':')[2].split('.')[0]]))
+
+print(saldo_final_positivo)
+# %%
+saldo_final_positivo
+#%%
+# Somar os valores do saldo para cada usuário no mês negativo
+mes_negativo['saldo'] = mes_negativo['saldo'].apply(lambda x: -pd.Timedelta(x).total_seconds() / 3600)
+saldo_final_negativo = mes_negativo.groupby('pin')['saldo'].sum().reset_index()
+
+# Converter o saldo final de volta para o formato hh:mm:ss
+saldo_final_negativo['saldo'] = saldo_final_negativo['saldo'].apply(lambda x: '-' + ':'.join(str(pd.Timedelta(hours=abs(x))).split(' ')[-1].split(':')[:2] + [str(pd.Timedelta(hours=abs(x))).split(' ')[-1].split(':')[2].split('.')[0]]))
+
+print(saldo_final_negativo)
+# %%
+saldo_final_negativo
+
+# %%
+# Mesclar os DataFrames de saldo positivo e negativo
+# Mesclar os DataFrames de saldo positivo e negativo
+saldo_final = pd.concat([saldo_final_positivo, saldo_final_negativo], ignore_index=True)
+saldo_final_positivo = saldo_final_positivo.rename(columns={'saldo': 'saldo_positivo'})
+saldo_final_negativo = saldo_final_negativo.rename(columns={'saldo': 'saldo_negativo'})
+
+saldo_final = pd.merge(saldo_final_positivo, saldo_final_negativo, on='pin', how='outer')
+
+print(saldo_final)
+# %%
+saldo_final
+# %%
+
+# %%
+# Converter as colunas de saldo para Timedelta
+
+
+saldo_final['saldo_positivo_horas'] = saldo_final['saldo_positivo'].apply(lambda x: pd.Timedelta(x).total_seconds() / 3600)
+saldo_final['saldo_negativo_horas'] = saldo_final['saldo_negativo'].apply(lambda x: pd.Timedelta(x).total_seconds() / 3600)
+
+# Somar os valores de saldo positivo e negativo em horas
+saldo_final['saldo_total_horas'] = saldo_final['saldo_positivo_horas'] + saldo_final['saldo_negativo_horas']
+
+# Converter saldo_total_horas para hh:mm:ss
+saldo_final['saldo_total'] = saldo_final['saldo_total_horas'].apply(lambda x: str(pd.Timedelta(hours=abs(x))).split(' ')[-1] if x >= 0 else '-' + str(pd.Timedelta(hours=abs(x))).split(' ')[-1])
+
+# Remove milliseconds
+saldo_final['saldo_total'] = saldo_final['saldo_total'].apply(lambda x: ':'.join(x.split(':')[:2] + [x.split(':')[2].split('.')[0]]))
+
+print(saldo_final[['pin', 'saldo_positivo_horas', 'saldo_negativo_horas', 'saldo_total']])
+# %%
+saldo_final
+# %%
+saldo_final[['pin', 'saldo_positivo', 'saldo_negativo', 'saldo_total']]
 # %%
